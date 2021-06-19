a0d1868e0bfb38b9f954503c6aa924ed4f432554
RootOperator <20143055+RootOperator@users.noreply.github.com>
2021-06-19 11:58:24 +0200

    more stuff

---
+++ .local/source/brightness.cpp
@@ -0,0 +1,72 @@
+#include <iostream>
+#include <unistd.h>
+#include <fstream>
+#include <string>
+
+template<typename T>
+void print(T message) {
+    std::cout << message << std::endl;
+}
+
+
+int main(int arc, char *argv[]) {
+    using namespace std;
+    string dir = "/sys/class/backlight/intel_backlight";
+    ifstream fileStream;
+    
+    fileStream.open(dir + "/brightness");
+
+    string max_brightness;
+    int brightness;
+    int new_brightness;
+    int increment = 200;
+
+    if (fileStream.is_open()) {
+        string n;
+        fileStream >> n;
+        brightness = stoi(n);
+        fileStream.close();
+        fileStream.open(dir +"/max_brightness");
+        
+        if (fileStream.is_open()) {
+            fileStream >> max_brightness;
+            fileStream.close();
+        }
+    }
+
+    if (arc >= 3) {
+        increment = stoi(argv[2]);
+    }
+
+    if (arc >= 2) {
+        string arg = argv[1];
+        if (arg == "-dec"){
+            new_brightness = brightness - increment;
+        } else if (arg == "-inc") {
+            new_brightness = brightness + increment;
+        } else if (arg == "-set") {
+            if (arc >= 3) {
+                new_brightness = stoi(argv[2]);
+            } else {
+                print("-set takes a integer between 0/" + max_brightness);
+            }
+        }
+
+        if (new_brightness < 0) {
+            new_brightness = 50;
+        } else if (new_brightness > stoi(max_brightness)) {
+            new_brightness = stoi(max_brightness);
+        }
+
+        ofstream fs;
+        fs.open(dir + "/brightness");
+        fs << new_brightness;
+        fs.close();
+    } else {
+        print("brightness takes an agument");
+    }
+     
+    return 0;
+}
+
+
--- .vimrc
+++ .vimrc
@@ -38,6 +38,24 @@ map <C-t> :vert term<CR>
 map ZX :noh<CR>
 map ZT :tab term<CR>
 
+
+
+"auto close {
+function! s:CloseBracket()
+    let line = getline('.')
+    if line =~# '^\s*\(struct\|class\|enum\) '
+        return "{\<Enter>};\<Esc>O"
+    elseif searchpair('(', '', ')', 'bmn', '', line('.'))
+        " Probably inside a function call. Close it off.
+        return "{\<Enter>});\<Esc>O"
+    else
+        return "{\<Enter>}\<Esc>O"
+    endif
+endfunction
+inoremap <expr> {<Enter> <SID>CloseBracket()
+
+
+
 "set background=dark
 color Atelier_SulphurpoolDark
 
