cdfaecc6fffb09717362b770e779498871c3a49d
RootOperator <20143055+RootOperator@users.noreply.github.com>
2021-06-20 17:22:35 +0200

    more stuff

--- .local/source/brightness/config.h
+++ .local/source/brightness/config.h
@@ -5,14 +5,14 @@
 /* Directory of the device */
 const std::string DIRECTORY = "/sys/class/backlight/intel_backlight/";
 
-/* Set default value for increading and decreasing the brightness. */
+/* Set default value for increasing and decreasing the brightness. */
 const int DEFAULT_CHANGE = 200;
 
-/* Set threshold for lowest brightness. */
+/* Set threshold for the lowest brightness. */
 const int MIN_THRESHOLD = 50;
 
 /* Set threshold for highest brightness.
- * Leave 0 for highest brightness
+ * Leave 0 for max_brightness
  *
  * NOTE: if you set it higher then the max_brightness (max_brightness
  * can be found in /sys/class/backlight/<device>/max_brightness)
